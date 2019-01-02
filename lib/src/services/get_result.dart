441637acde41036f24ee1383293f200639675a20
CoMatu <30466272+CoMatu@users.noreply.github.com>
2019-01-02 11:25:14 +0500

    продолжаю писать логику

--- lib/src/services/get_result.dart
+++ lib/src/services/get_result.dart
@@ -1,9 +1,29 @@
 // начинаю писать с хвоста теста, в этом сервисе будем вычислять результат тестирования
-class getResult {
-  //используем массив с результатами теста в виде map
-  getResult(Map result_map) {
-    Map test_result = Map();
-    test_result = null;
-      return test_result;
+class GetResult {
+
+//  List<int> result_list_P = List(); //массив для записи результатов тестов P
+//  List<int> result_list_A = List(); //массив для записи результатов тестов A
+//  List<int> result_list_E = List(); //массив для записи результатов тестов E
+//  List<int> result_list_I = List(); //массив для записи результатов тестов I
+
+  getResult(List result_list_P, List result_list_A, List result_list_E, List result_list_I) {
+
+    Map test_result = Map(); //массив для вывода результатов PAEI
+
+    test_result['P'] = getSumm(result_list_P);
+    test_result['A'] = getSumm(result_list_A);
+    test_result['E'] = getSumm(result_list_E);
+    test_result['I'] = getSumm(result_list_I);
+
+    print(test_result.toString()); //смотрим в консоли что получилось
+
+    return test_result;
+  }
+  getSumm(List result_list) {
+    int summ = 0;
+    for(int i=0; i<12; i++){
+      summ += result_list[i];
+    }
+    return summ;
   }
 }
--- lib/src/test_forms/test_form_component.dart
+++ lib/src/test_forms/test_form_component.dart
@@ -2,6 +2,7 @@
 
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
+import 'package:adizis_test_app/src/services/get_result.dart';
 
 @Component(
   selector: 'test-forms',
@@ -16,5 +17,9 @@ import 'package:angular_components/angular_components.dart';
 )
 
 class TestFormComponent {
-  
+  List<int> result_list_P = List(); //массив для записи результатов тестов P
+  List<int> result_list_A = List(); //массив для записи результатов тестов A
+  List<int> result_list_E = List(); //массив для записи результатов тестов E
+  List<int> result_list_I = List(); //массив для записи результатов тестов I
+
 }
